bb250503395ed952b3d84c9f1cd20c22dfc8f334
Heejong Ahn <heejongahn@gmail.com>
2018-10-07 20:27:58 +0900

    Replace push with navigateTo

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { push } from "gatsby";
+import { navigateTo } from "gatsby";
 
 const ArticlesMain: React.SFC = () => {
-  push("/");
+  navigateTo("/");
   return null;
 };
 
